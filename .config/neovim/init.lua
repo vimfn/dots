72b8ddd24090a84f55f957890813bec970990a7c
Arunava Ghosh <102473837+its-ag@users.noreply.github.com>
2023-02-15 17:43:05 +0530

    feat: nvim

---
+++ .config/neovim/init.lua
@@ -0,0 +1,9 @@
+-- https://github.com/its-ag
+
+-- Fix pasting emojis
+vim.cmd [[let $LANG="en_US.UTF-8"]]
+
+require("plugins")
+require("options")
+require("configs")
+require("keybinds")
---
+++ .config/neovim/lua/configs/lsp.lua
@@ -0,0 +1,33 @@
+--------------------------------------------------------------------
+--                           LSP                                  --
+--------------------------------------------------------------------
+
+require("mason").setup({
+    ensure_installed = { "sumneko_lua", "tsserver", "pyright" },
+    automatic_installation = true
+})
+
+require("mason-lspconfig").setup()
+
+require("mason-lspconfig").setup_handlers {
+    -- The first entry (without a key) will be the default handler
+    -- and will be called for each installed server that doesn't have
+    -- a dedicated handler.
+    function(server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup {}
+    end,
+    -- Next, you can provide a dedicated handler for specific servers.
+    -- For example, a handler override for the `rust_analyzer`:
+    ["sumneko_lua"] = function()
+        require('lspconfig').sumneko_lua.setup({
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim", "use" },
+                        disable = { "lowercase-global" }
+                    }
+                }
+            }
+        })
+    end
+}
